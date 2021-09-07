341b6a8d8a217ba1f96725eed543a9fc7b66c869
Antonio Giler <agiler1699@gmail.com>
2021-09-06 23:48:46 -0500

    cambios

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/web-modelamiento-simulacion'));
+app.use(express.static(__dirname + '/dist/webModelamientoSimulacion'));
 app.get('/*', function (req, res) {
-	res.sendFile(path.join(__dirname + '/dist/web-modelamiento-simulacion/index.html'));
+	res.sendFile(path.join(__dirname + '/dist/webModelamientoSimulacion/index.html'));
 });
 app.listen(process.env.PORT || 8080);
