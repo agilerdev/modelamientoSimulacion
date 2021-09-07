305a27dd30731ad60a08ac167d081c18c68e3e9c
Antonio Giler <agiler1699@gmail.com>
2021-09-06 23:37:38 -0500

    cambios

--- server.js
+++ server.js
@@ -1,15 +1,8 @@
-function requireHTTPS(req, res, next) {
-	if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
-		return res.redirect('https://' + req.get('host') + req.url);
-	}
-	next();
-}
 const express = require('express');
+const path = require('path');
 const app = express();
-app.use(requireHTTPS);
-app.use(express.static(`./dist/web-modelamiento-simulacion`));
-app.get(`/*`, function (req, res) {
-	res.sendFile(`index.html`, { root: `dist/web-modelamiento-simulacion/` }
-	);
+app.use(express.static(__dirname + '/dist/web-modelamiento-simulacion'));
+app.get('/*', function (req, res) {
+	res.sendFile(path.join(__dirname + '/dist/web-modelamiento-simulacion/index.html'));
 });
 app.listen(process.env.PORT || 8080);
